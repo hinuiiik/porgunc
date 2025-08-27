42acc6dd8e352f089fb543f67b4616a19c7991ac
Vikram <hello@vikramk.dev>
2025-08-27 17:04:12 -0400

    Update logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -17,13 +17,15 @@ export const Logo = (props: Props) => {
     /* eslint-disable @next/next/no-img-element */
     <img
       alt="Payload Logo"
-      width={193}
-      height={34}
+      width={60}
+      height={60}
       loading={loading}
       fetchPriority={priority}
       decoding="async"
-      className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
-      src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      // className={clsx('max-w-[9.375rem] w-full h-[34px]', className)}
+      className={clsx('max-w-[9.375rem] w-full h-[60px]', className)}
+      // src="https://raw.githubusercontent.com/payloadcms/payload/main/packages/ui/src/assets/payload-logo-light.svg"
+      src={"/porgplaceholder.svg"}
     />
   )
 }
