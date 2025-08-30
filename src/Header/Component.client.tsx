2612585e6be1a8e7dca2a1eae744481afb00110d
Vikram <hello@vikramk.dev>
2025-08-29 22:35:36 -0400

    Fix search

--- src/Header/Component.client.tsx
+++ src/Header/Component.client.tsx
@@ -1,15 +1,12 @@
 'use client'
 import { useHeaderTheme } from '@/providers/HeaderTheme'
-import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React, { useEffect, useState } from 'react'
 
 import type { Header } from '@/payload-types'
 
-import { Logo } from '@/components/Logo/Logo'
-import { HeaderNav } from './Nav'
-import { Navbar02 } from '@/components/ui/shadcn-io/navbar-02'
 import { Navbar08, Navbar08NavItem } from '@/components/ui/shadcn-io/navbar-08'
+import { useRouter } from 'next/navigation'
 
 interface HeaderClientProps {
   data: Header
@@ -20,8 +17,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
   const [theme, setTheme] = useState<string | null>(null)
   const { headerTheme, setHeaderTheme } = useHeaderTheme()
   const pathname = usePathname()
-  console.log(data.navItems?.at(1)?.link.label);
-  console.log(data.navItems?.at(1)?.link.url);
+  const router = useRouter()
 
   useEffect(() => {
     setHeaderTheme(null)
@@ -43,7 +39,7 @@ export const HeaderClient: React.FC<HeaderClientProps> = ({ data }) => {
 
   return (
     <div className="relative w-full">
-      <Navbar08 navigationLinks={userNavigationLinks} />
+      <Navbar08 navigationLinks={userNavigationLinks} logoHref={"/home"} onSearchSubmit={(query) => router.push(`/search?q=${encodeURIComponent(query)}`)} />
     </div>
   )
 }
