98486beecd6851c8da7ddd78dc324da92e0b3893
Vikram <hello@vikramk.dev>
2025-10-08 14:10:27 -0400

    Fix category redirect await

--- src/app/(frontend)/posts/category/[categorySlug]/page.tsx
+++ src/app/(frontend)/posts/category/[categorySlug]/page.tsx
@@ -1,12 +1,6 @@
 import { redirect } from 'next/navigation'
 
-type Props = {
-  params: {
-    categorySlug: string
-  }
-}
-
-export default async function CategoryRedirect({ params }: Props) {
-  const { categorySlug } = await params
+export default async function CategoryRedirect({ params }: {params: Promise<{ categorySlug: string }>}) {
+  const { categorySlug } = await params;
   return redirect(`/posts/category/${categorySlug}/1`)
 }
