503e8ef7dd179ced95132b6a23c4d3ba7076fb2a
Vikram <hello@vikramk.dev>
2025-10-30 11:10:48 -0400

    Remove migrations

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,5 @@
-import * as migration_20251030_043707_polls_and_posts_separation from './20251030_043707_polls_and_posts_separation';
 
 export const migrations = [
   {
-    up: migration_20251030_043707_polls_and_posts_separation.up,
-    down: migration_20251030_043707_polls_and_posts_separation.down,
-    name: '20251030_043707_polls_and_posts_separation'
   },
 ];
